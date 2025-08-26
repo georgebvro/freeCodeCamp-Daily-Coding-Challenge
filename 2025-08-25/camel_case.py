1dd88ef5a72d9934ebcbea1a49f1b5237f8aac08
GxG <georgemailro@yahoo.com>
2025-08-26 14:38:40 +0300

    fix: remove unnecessary semicolons

--- 2025-08-25/camel_case.py
+++ 2025-08-25/camel_case.py
@@ -10,8 +10,8 @@ def to_camel_case(s):
 
     return ''.join(words)
 
-print(to_camel_case("hello world"));
-print(to_camel_case("HELLO WORLD"));
-print(to_camel_case("secret agent-X"));
-print(to_camel_case("FREE cODE cAMP"));
-print(to_camel_case("ye old-_-sea  faring_buccaneer_-_with a - peg__leg----and a_parrot_ _named- _squawk"));
+print(to_camel_case("hello world"))
+print(to_camel_case("HELLO WORLD"))
+print(to_camel_case("secret agent-X"))
+print(to_camel_case("FREE cODE cAMP"))
+print(to_camel_case("ye old-_-sea  faring_buccaneer_-_with a - peg__leg----and a_parrot_ _named- _squawk"))
