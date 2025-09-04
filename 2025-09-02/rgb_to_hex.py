52553ad570e092d0c266c49fcb97d993bfb08d1a
GxG <georgemailro@yahoo.com>
2025-09-04 19:52:31 +0300

    fix: correct Python variable names to adhere to naming conventions

--- 2025-09-02/rgb_to_hex.py
+++ 2025-09-02/rgb_to_hex.py
@@ -3,9 +3,9 @@ import re
 def rgb_to_hex(rgb):
     regex = re.compile(r"rgb\( *(?P<red>\d{1,3}) *, *(?P<green>\d{1,3}) *, *(?P<blue>\d{1,3}) *\)")
 
-    regexResult = re.match(regex, rgb)
+    regex_result = re.match(regex, rgb)
 
-    return f"#{int(regexResult.group('red')):02x}{int(regexResult.group('green')):02x}{int(regexResult.group('blue')):02x}"
+    return f"#{int(regex_result.group('red')):02x}{int(regex_result.group('green')):02x}{int(regex_result.group('blue')):02x}"
 
 print(rgb_to_hex("rgb(255, 255, 255)"))
 print(rgb_to_hex("rgb(1, 11, 111)"))
