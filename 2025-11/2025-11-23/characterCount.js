1046fe709271fd8ac7b8286c55c29eeebb444193
GxG <georgemailro@yahoo.com>
2025-11-27 10:44:51 +0200

    feat: add challenge for November 23, 2025 - Character Count

---
+++ 2025-11/2025-11-23/characterCount.js
@@ -0,0 +1,66 @@
+function countCharacters(sentence) {
+  const characters = Array.from(new Set(sentence.toLowerCase().replace(/[^a-z]/g, "")));
+  
+  const characterCount = characters.map(character => {
+    const regex = new RegExp(character, "gi");
+    const count = sentence.match(regex).length;
+
+    return `${character} ${count}`;
+  })
+
+  return characterCount.sort();
+}
+
+// --- TEST SUITE ---
+
+const testsText = `
+1. countCharacters("hello world") should return ["d 1", "e 1", "h 1", "l 3", "o 2", "r 1", "w 1"].
+2. countCharacters("I love coding challenges!") should return ["a 1", "c 2", "d 1", "e 3", "g 2", "h 1", "i 2", "l 3", "n 2", "o 2", "s 1", "v 1"].
+3. countCharacters("// TODO: Complete this challenge ASAP!") should return ["a 3", "c 2", "d 1", "e 4", "g 1", "h 2", "i 1", "l 3", "m 1", "n 1", "o 3", "p 2", "s 2", "t 3"].
+`;
+
+const testsRegex = /(?<number>\d+)\.\s(?<functionCall>.+) should return (?<output>.+)\.$/gm;
+const testData = [...testsText.matchAll(testsRegex).map(match => match.groups)];
+
+function runTests(testData) {
+  console.log("--------------------------");
+  console.log("🧪Starting Verification...");
+  console.log("--------------------------");
+
+  let allPassed = true;
+  let failCount = 0;
+  
+  testData.forEach(test => {
+    const functionCallOutput = eval(test.functionCall);
+    const testOutput = eval(test.output);
+
+    if (arraysEqual(functionCallOutput, testOutput)) {
+      console.log(`${test.number}.✅PASS - Function Call: ${test.functionCall}`);
+    }
+    else {
+      console.log(`${test.number}.❌FAIL - Function Call: ${test.functionCall}\nExpected: ${testOutput}\nGot: ${functionCallOutput}`);
+      allPassed = false;
+      failCount ++;
+    }
+  })
+
+  console.log("----------------------------");
+
+  if (allPassed)
+    console.log("🎉SUCCESS: All tests PASSED.");
+  else
+    console.log(`⚠️WARNING: ${failCount}/${testData.length} tests FAILED.`);
+}
+
+function arraysEqual(a, b) {
+  if (a.length !== b.length)
+    return false;
+
+  for (const index in a)
+    if (a[index] !== b[index])
+      return false;
+  
+  return true;
+}
+
+runTests(testData);
---
+++ 2025-11/2025-11-23/character_count.py
@@ -0,0 +1,44 @@
+def count_characters(sentence):
+    characters = set(re.sub("[^a-z]", "", sentence.lower()))
+
+    return sorted([f"{character} {len(re.findall(f'(?i){character}', sentence))}" for character in characters])
+
+tests_text = '''
+Waiting:1. count_characters("hello world") should return ["d 1", "e 1", "h 1", "l 3", "o 2", "r 1", "w 1"].
+Waiting:2. count_characters("I love coding challenges!") should return ["a 1", "c 2", "d 1", "e 3", "g 2", "h 1", "i 2", "l 3", "n 2", "o 2", "s 1", "v 1"].
+Waiting:3. count_characters("// TODO: Complete this challenge ASAP!") should return ["a 3", "c 2", "d 1", "e 4", "g 1", "h 2", "i 1", "l 3", "m 1", "n 1", "o 3", "p 2", "s 2", "t 3"].
+'''
+
+import re
+
+tests_regex = re.compile(r"(?P<number>\d+)\.\s(?P<function_call>.+) should return (?P<output>.+)\.$", re.MULTILINE)
+test_data = [item.groupdict() for item in re.finditer(tests_regex, tests_text)]
+
+def run_tests(test_data):
+    print("--------------------------")
+    print("🧪 Starting Verification...")
+    print("--------------------------")
+
+    all_passed = True
+    fail_count = 0
+
+    for test in test_data:
+        print("test['function_call']:", test['function_call'])
+        function_call_output = eval(test['function_call'])
+        test_output = eval(test['output'])
+        
+        if function_call_output == test_output:
+            print(f"{test['number']}.✅ PASS - Function Call:\n{test['function_call']}")
+        else:
+            print(f"{test['number']}.❌ FAIL - Function Call:\n{test['function_call']}\nExpected:\n{test_output}\nGot:\n{function_call_output}")
+            all_passed = False
+            fail_count += 1
+
+    print("----------------------------");
+
+    if (all_passed):
+        print("🎉 SUCCESS: All tests PASSED.");
+    else:
+        print(f"⚠️ WARNING: {fail_count}/{len(test_data)} tests FAILED.");
+
+run_tests(test_data)
