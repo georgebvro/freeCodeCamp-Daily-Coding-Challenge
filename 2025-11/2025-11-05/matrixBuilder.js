e007fb1f4af540c63fd1ef35704f89c6b76a17fe
GxG <georgemailro@yahoo.com>
2025-12-08 11:25:10 +0200

    test: add tests for challenge of November 5, 2025

--- 2025-11/2025-11-05/matrixBuilder.js
+++ 2025-11/2025-11-05/matrixBuilder.js
@@ -12,3 +12,74 @@ console.log(buildMatrix(2, 3));
 console.log(buildMatrix(3, 2));
 console.log(buildMatrix(4, 3));
 console.log(buildMatrix(9, 1));
+
+// --- TEST SUITE ---
+
+const testsText = `
+1. buildMatrix(2, 3) should return [[0, 0, 0], [0, 0, 0]].
+2. buildMatrix(3, 2) should return [[0, 0], [0, 0], [0, 0]].
+3. buildMatrix(4, 3) should return [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]].
+4. buildMatrix(9, 1) should return [[0], [0], [0], [0], [0], [0], [0], [0], [0]].
+`;
+
+const testsRegex = /(?<number>\d+)\.\s(?<functionCall>.+) should return (?<output>.+)\.$/gm;
+const testData = [...testsText.matchAll(testsRegex).map(match => match.groups)];
+
+function runTests(testData) {
+  console.log("--------------------------");
+  console.log("🧪Starting Verification...");
+  console.log("--------------------------");
+
+  let allPassed = true;
+  let failCount = 0;
+  
+  testData.forEach(test => {
+    const functionCallOutput = eval(test.functionCall);
+    const testOutput = eval(test.output);
+    let comparison;
+
+    if (Array.isArray(testOutput))
+      comparison = arraysEqual(functionCallOutput, testOutput);
+    else
+      comparison = functionCallOutput === testOutput;
+
+    if (comparison) {
+      console.log(`${test.number}.✅PASS - Function Call: ${test.functionCall}`);
+    }
+    else {
+      console.log(`${test.number}.❌FAIL - Function Call: ${test.functionCall}\nExpected: ${testOutput}\nGot: ${functionCallOutput}`);
+      allPassed = false;
+      failCount ++;
+    }
+  })
+
+  console.log("----------------------------");
+
+  if (allPassed)
+    console.log("🎉SUCCESS: All tests PASSED.");
+  else
+    console.log(`⚠️WARNING: ${failCount}/${testData.length} tests FAILED.`);
+}
+
+function arraysEqual(a, b) {
+  if (a.length !== b.length)
+    return false;
+
+  for (const index in a) {
+    if (Array.isArray(a[index])) {
+      if (Array.isArray(b[index])) {
+        if (!arraysEqual(a[index], b[index]))
+          return false;
+      }
+      else 
+        return false;
+    }
+
+    else if (a[index] !== b[index])
+      return false;
+  }
+
+  return true;
+}
+
+runTests(testData);
--- 2025-11/2025-11-05/matrix_builder.py
+++ 2025-11/2025-11-05/matrix_builder.py
@@ -7,3 +7,45 @@ print(build_matrix(2, 3))
 print(build_matrix(3, 2))
 print(build_matrix(4, 3))
 print(build_matrix(9, 1))
+
+# --- TEST SUITE ---
+
+tests_text = '''
+Waiting:1. build_matrix(2, 3) should return [[0, 0, 0], [0, 0, 0]].
+Waiting:2. build_matrix(3, 2) should return [[0, 0], [0, 0], [0, 0]].
+Waiting:3. build_matrix(4, 3) should return [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]].
+Waiting:4. build_matrix(9, 1) should return [[0], [0], [0], [0], [0], [0], [0], [0], [0]].
+'''
+
+import re
+
+tests_regex = re.compile(r"(?P<number>\d+)\.\s(?P<function_call>.+) should return (?P<output>.+)\.$", re.MULTILINE)
+test_data = [item.groupdict() for item in re.finditer(tests_regex, tests_text)]
+
+def run_tests(test_data):
+    print("--------------------------")
+    print("🧪 Starting Verification...")
+    print("--------------------------")
+
+    all_passed = True
+    fail_count = 0
+
+    for test in test_data:
+        function_call_output = eval(test['function_call'])
+        test_output = eval(test['output'])
+        
+        if function_call_output == test_output:
+            print(f"{test['number']}.✅ PASS - Function Call:\n{test['function_call']}")
+        else:
+            print(f"{test['number']}.❌ FAIL - Function Call:\n{test['function_call']}\nExpected:\n{test_output}\nGot:\n{function_call_output}")
+            all_passed = False
+            fail_count += 1
+
+    print("----------------------------");
+
+    if (all_passed):
+        print("🎉 SUCCESS: All tests PASSED.");
+    else:
+        print(f"⚠️ WARNING: {fail_count}/{len(test_data)} tests FAILED.");
+
+run_tests(test_data)
