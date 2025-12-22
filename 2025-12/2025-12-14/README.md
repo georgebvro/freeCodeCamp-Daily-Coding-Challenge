7a73c69b48d0be63d3c3e240fa5b0cc1ff54d206
GxG <georgemailro@yahoo.com>
2025-12-22 12:58:56 +0200

    feat: add challenge for December 14, 2025 - Capitalize It

---
+++ 2025-12/2025-12-14/README.md
@@ -0,0 +1,15 @@
+# freeCodeCamp Daily Coding Challenge - December 14, 2025
+
+## Capitalize It
+
+Given a string title, return a new string formatted in title case using the following rules:
+* Capitalize the first letter of each word.
+* Make all other letters in each word lowercase.
+* Words are always separated by a single space.
+
+### Tests
+
+1. `titleCase("hello world")` should return `"Hello World"`.
+2. `titleCase("the quick brown fox")` should return `"The Quick Brown Fox"`.
+3. `titleCase("JAVASCRIPT AND PYTHON")` should return `"Javascript And Python"`.
+4. `titleCase("AvOcAdO tOAst fOr brEAkfAst")` should return `"Avocado Toast For Breakfast"`.
---
+++ 2025-12/2025-12-14/titleCase.js
@@ -0,0 +1,79 @@
+function titleCase(title) {
+
+  return title
+    .toLowerCase()
+    .split(" ")
+    .map(word => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+}
+
+// --- TEST SUITE ---
+
+const testsText = `
+1. titleCase("hello world") should return "Hello World".
+2. titleCase("the quick brown fox") should return "The Quick Brown Fox".
+3. titleCase("JAVASCRIPT AND PYTHON") should return "Javascript And Python".
+4. titleCase("AvOcAdO tOAst fOr brEAkfAst") should return "Avocado Toast For Breakfast".
+`;
+
+const testsRegex = /(?<number>\d+)\.\s(?<functionCall>.+) should return (?<output>.+?)\.?$/gm;
+const testData = [...testsText.matchAll(testsRegex).map(match => match.groups)];
+
+function runTests(testData) {
+  console.log("--------------------------");
+  console.log("🧪Starting Verification...");
+  console.log("--------------------------");
+
+  let allPassed = true;
+  let failCount = 0;
+  
+  testData.forEach(test => {
+    const functionCallOutput = eval(test.functionCall);
+    const testOutput = eval(test.output);
+    let comparison;
+
+    if (Array.isArray(testOutput))
+      comparison = arraysEqual(functionCallOutput, testOutput);
+    else
+      comparison = functionCallOutput === testOutput;
+
+    if (comparison) {
+      console.log(`${test.number}.✅PASS - Function Call: ${test.functionCall}`);
+    }
+    else {
+      console.log(`${test.number}.❌FAIL - Function Call: ${test.functionCall}\nExpected: ${testOutput}\nGot: ${functionCallOutput}`);
+      allPassed = false;
+      failCount ++;
+    }
+  })
+
+  console.log("----------------------------");
+
+  if (allPassed)
+    console.log("🎉SUCCESS: All tests PASSED.");
+  else
+    console.log(`⚠️WARNING: ${failCount}/${testData.length} tests FAILED.`);
+}
+
+function arraysEqual(a, b) {
+  if (a.length !== b.length)
+    return false;
+
+  for (let i = 0; i < a.length; ++i) {
+    if (Array.isArray(a[i])) {
+      if (Array.isArray(b[i])) {
+        if (!arraysEqual(a[i], b[i]))
+          return false;
+      }
+      else 
+        return false;
+    }
+
+    else if (a[i] !== b[i])
+      return false;
+  }
+
+  return true;
+}
+
+runTests(testData);
