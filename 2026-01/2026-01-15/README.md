805afa7e6525b0f9034b7daf7069f227d27cad62
GxG <georgemailro@yahoo.com>
2026-01-27 11:10:54 +0200

    add challenge for January 15, 2026 - Array Swap

---
+++ 2026-01/2026-01-15/README.md
@@ -0,0 +1,14 @@
+# freeCodeCamp Daily Coding Challenge - January 15, 2026
+
+## Array Swap
+
+Given an array with two values, return an array with the values swapped.
+
+For example, given `["A", "B"]` return `["B", "A"]`.
+
+### Tests
+
+1. `arraySwap(["A", "B"])` should return `["B", "A"]`.
+2. `arraySwap([25, 20])` should return `[20, 25]`.
+3. `arraySwap([true, false])` should return `[false, true]`.
+4. `arraySwap(["1", 1])` should return `[1, "1"]`.
---
+++ 2026-01/2026-01-15/array_swap.py
@@ -0,0 +1,41 @@
+def array_swap(arr):
+
+    return arr[::-1]
+
+# --- TEST SUITE ---
+
+tests_text = r'''
+1. array_swap(["A", "B"]) should return ["B", "A"].
+2. array_swap([25, 20]) should return [20, 25].
+3. array_swap([True, False]) should return [False, True].
+4. array_swap(["1", 1]) should return [1, "1"].
+'''
+
+import re
+
+tests_regex = re.compile(r"(?P<number>\d+)\.\s(?P<function_call>.+) should return (?P<output>.+?)\.?$", re.MULTILINE)
+test_data = [item.groupdict() for item in re.finditer(tests_regex, tests_text)]
+
+def run_tests(test_data):
+    print("--------------------------")
+    print("🧪 Starting Verification...")
+    print("--------------------------")
+
+    fail_count = 0
+
+    for test in test_data:
+        function_call_output = eval(test['function_call'])
+        test_output = eval(test['output'])
+        
+        if function_call_output == test_output:
+            print(f"{test['number']}.✅ PASS - Function Call:\n{test['function_call']}")
+        else:
+            print(f"{test['number']}.❌ FAIL - Function Call:\n{test['function_call']}\nExpected:\n{test_output}\nGot:\n{function_call_output}")
+            fail_count += 1
+
+    print("----------------------------",
+        f"\n⚠️ WARNING: {fail_count}/{len(test_data)} tests FAILED." if fail_count
+        else "\n🎉 SUCCESS: All tests PASSED."
+    )
+
+run_tests(test_data)
