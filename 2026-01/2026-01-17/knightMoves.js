d3efc62d9dc01e760a65391be8eb3b699c67d87a
GxG <georgemailro@yahoo.com>
2026-01-27 14:47:57 +0200

    add challenge for January 17, 2026 - Knight Moves

---
+++ 2026-01/2026-01-17/knightMoves.js
@@ -0,0 +1,97 @@
+function knightMoves(position) {
+  const boardSize = 8;
+
+  const startPositionIndeces = 
+    new BoardIndeces(boardSize - position[1], position[0].charCodeAt() - 'A'.charCodeAt());
+
+  const potentialMovePositions = [
+    new BoardIndeces(startPositionIndeces.row + 1, startPositionIndeces.column - 2),
+    new BoardIndeces(startPositionIndeces.row - 1, startPositionIndeces.column - 2),
+    new BoardIndeces(startPositionIndeces.row - 2, startPositionIndeces.column - 1),
+    new BoardIndeces(startPositionIndeces.row - 2, startPositionIndeces.column + 1),
+    new BoardIndeces(startPositionIndeces.row - 1, startPositionIndeces.column + 2),
+    new BoardIndeces(startPositionIndeces.row + 1, startPositionIndeces.column + 2),
+    new BoardIndeces(startPositionIndeces.row + 2, startPositionIndeces.column - 1),
+    new BoardIndeces(startPositionIndeces.row + 2, startPositionIndeces.column + 1),
+  ];
+
+  return potentialMovePositions.filter(potentialMovePosition => 
+    0 <= potentialMovePosition.row && potentialMovePosition.row < boardSize
+    && 
+    0 <= potentialMovePosition.column && potentialMovePosition.column < boardSize
+  ).length;
+}
+
+class BoardIndeces {
+  constructor(row, column) {
+    this.row = row;
+    this.column = column;
+  }
+}
+
+// --- TEST SUITE ---
+
+const testsText = String.raw`
+1. knightMoves("A1") should return 2.
+2. knightMoves("D4") should return 8.
+3. knightMoves("G6") should return 6.
+4. knightMoves("B8") should return 3.
+5. knightMoves("H3") should return 4.
+`;
+
+const testsRegex = /(?<number>\d+)\.\s(?<functionCall>.+) should return (?<output>.+?)\.?$/gm;
+const testData = [...testsText.matchAll(testsRegex).map(match => match.groups)];
+
+function runTests(testData) {
+  console.log("--------------------------");
+  console.log("🧪Starting Verification...");
+  console.log("--------------------------");
+
+  let failCount = 0;
+  
+  testData.forEach(test => {
+    const functionCallOutput = eval(test.functionCall);
+    const testOutput = eval(test.output);
+
+    const comparison = Array.isArray(testOutput)
+      ? arraysEqual(functionCallOutput, testOutput)
+      : functionCallOutput === testOutput;
+
+    if (comparison) {
+      console.log(`${test.number}.✅PASS - Function Call: ${test.functionCall}`);
+    }
+    else {
+      console.log(`${test.number}.❌FAIL - Function Call: ${test.functionCall}\nExpected: ${testOutput}\nGot: ${functionCallOutput}`);
+      ++failCount;
+    }
+  })
+
+  console.log("----------------------------",
+    failCount
+    ? `\n⚠️WARNING: ${failCount}/${testData.length} tests FAILED.`
+    : "\n🎉SUCCESS: All tests PASSED."
+  );
+}
+
+function arraysEqual(a, b) {
+  if (a.length !== b.length)
+    return false;
+
+  for (let i = 0; i < a.length; ++i) {
+    if (Array.isArray(a[i])) {
+      if (Array.isArray(b[i])) {
+        if (!arraysEqual(a[i], b[i]))
+          return false;
+      }
+      else 
+        return false;
+    }
+
+    else if (a[i] !== b[i])
+      return false;
+  }
+
+  return true;
+}
+
+runTests(testData);
---
+++ 2026-01/2026-01-17/knight_moves_class.py
@@ -0,0 +1,66 @@
+# Solution using objects of a custom class to store knight positions on the board
+
+def knight_moves(position):
+    board_size = 8
+
+    start_position_indeces = BoardIndeces(board_size - int(position[1]), ord(position[0]) - ord('A'))
+
+    potential_move_positions = [
+        BoardIndeces(start_position_indeces.row + 1, start_position_indeces.column - 2),
+        BoardIndeces(start_position_indeces.row - 1, start_position_indeces.column - 2),
+        BoardIndeces(start_position_indeces.row - 2, start_position_indeces.column - 1),
+        BoardIndeces(start_position_indeces.row - 2, start_position_indeces.column + 1),
+        BoardIndeces(start_position_indeces.row - 1, start_position_indeces.column + 2),
+        BoardIndeces(start_position_indeces.row + 1, start_position_indeces.column + 2),
+        BoardIndeces(start_position_indeces.row + 2, start_position_indeces.column - 1),
+        BoardIndeces(start_position_indeces.row + 2, start_position_indeces.column + 1),
+    ]
+
+    return len([potential_move_position for potential_move_position in potential_move_positions 
+        if 0 <= potential_move_position.row < board_size 
+        and 0 <= potential_move_position.column < board_size
+    ])
+
+class BoardIndeces:
+    def __init__(self, row, column):
+        self.row = row
+        self.column = column
+
+# --- TEST SUITE ---
+
+tests_text = r'''
+1. knight_moves("A1") should return 2.
+2. knight_moves("D4") should return 8.
+3. knight_moves("G6") should return 6.
+4. knight_moves("B8") should return 3.
+5. knight_moves("H3") should return 4.
+'''
+
+import re
+
+tests_regex = re.compile(r"(?P<number>\d+)\.\s(?P<function_call>.+) should return (?P<output>.+?)\.?$", re.MULTILINE)
+test_data = [item.groupdict() for item in re.finditer(tests_regex, tests_text)]
+
+def run_tests(test_data):
+    print("--------------------------")
+    print("🧪 Starting Verification...")
+    print("--------------------------")
+
+    fail_count = 0
+
+    for test in test_data:
+        function_call_output = eval(test['function_call'])
+        test_output = eval(test['output'])
+        
+        if function_call_output == test_output:
+            print(f"{test['number']}.✅ PASS - Function Call:\n{test['function_call']}")
+        else:
+            print(f"{test['number']}.❌ FAIL - Function Call:\n{test['function_call']}\nExpected:\n{test_output}\nGot:\n{function_call_output}")
+            fail_count += 1
+
+    print("----------------------------",
+        f"\n⚠️ WARNING: {fail_count}/{len(test_data)} tests FAILED." if fail_count
+        else "\n🎉 SUCCESS: All tests PASSED."
+    )
+
+run_tests(test_data)
