833894dffbc751590e9ac4d27eb2d6e09829c7c4
GxG <georgemailro@yahoo.com>
2026-02-03 16:09:20 +0200

    add challenge for January 23, 2026 - Hex Validator

---
+++ 2026-01/2026-01-23/README.md
@@ -0,0 +1,20 @@
+# freeCodeCamp Daily Coding Challenge - January 23, 2026
+
+## Hex Validator
+
+Given a string, determine whether it is a valid CSS hex color. A valid CSS hex color must:
+* Start with a `#`, and
+* be followed by either 3 or 6 hexadecimal characters.
+
+Hexadecimal characters are numbers `0` through `9` and letters `a` through `f` (case-insensitive).
+
+### Tests
+
+1. `isValidHex("#123")` should return `true`.
+2. `isValidHex("#123abc")` should return `true`.
+3. `isValidHex("#ABCDEF")` should return `true`.
+4. `isValidHex("#0a1B2c")` should return `true`.
+5. `isValidHex("#12G")` should return `false`.
+6. `isValidHex("#1234567")` should return `false`.
+7. `isValidHex("#12 3")` should return `false`.
+8. `isValidHex("fff")` should return `false`.
---
+++ 2026-01/2026-01-23/hexValidator.js
@@ -0,0 +1,74 @@
+function isValidHex(str) {
+
+  return Boolean(str.match(/^#([\dA-F]{6}|[\dA-F]{3})$/gi));
+}
+
+// --- TEST SUITE ---
+
+const testsText = String.raw`
+1. isValidHex("#123") should return true.
+2. isValidHex("#123abc") should return true.
+3. isValidHex("#ABCDEF") should return true.
+4. isValidHex("#0a1B2c") should return true.
+5. isValidHex("#12G") should return false.
+6. isValidHex("#1234567") should return false.
+7. isValidHex("#12 3") should return false.
+8. isValidHex("fff") should return false.
+`;
+
+const testsRegex = /(?<number>\d+)\.\s(?<functionCall>.+) should return (?<output>.+?)\.?$/gm;
+const testData = [...testsText.matchAll(testsRegex).map(match => match.groups)];
+
+function runTests(testData) {
+  console.log("--------------------------");
+  console.log("🧪Starting Verification...");
+  console.log("--------------------------");
+
+  let failCount = 0;
+  
+  testData.forEach(test => {
+    const functionCallOutput = eval(test.functionCall);
+    const testOutput = eval(test.output);
+
+    const comparison = Array.isArray(testOutput)
+      ? arraysEqual(functionCallOutput, testOutput)
+      : functionCallOutput === testOutput;
+
+    if (comparison) {
+      console.log(`${test.number}.✅PASS - Function Call: ${test.functionCall}`);
+    }
+    else {
+      console.log(`${test.number}.❌FAIL - Function Call: ${test.functionCall}\nExpected: ${testOutput}\nGot: ${functionCallOutput}`);
+      ++failCount;
+    }
+  })
+
+  console.log("----------------------------",
+    failCount
+    ? `\n⚠️WARNING: ${failCount}/${testData.length} tests FAILED.`
+    : "\n🎉SUCCESS: All tests PASSED."
+  );
+}
+
+function arraysEqual(a, b) {
+  if (a.length !== b.length)
+    return false;
+
+  for (let i = 0; i < a.length; ++i) {
+    if (Array.isArray(a[i])) {
+      if (Array.isArray(b[i])) {
+        if (!arraysEqual(a[i], b[i]))
+          return false;
+      }
+      else 
+        return false;
+    }
+
+    else if (a[i] !== b[i])
+      return false;
+  }
+
+  return true;
+}
+
+runTests(testData);
