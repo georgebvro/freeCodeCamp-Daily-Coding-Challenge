b15740655c97c281d931deb41a7fcd3bf28824a9
GxG <georgemailro@yahoo.com>
2026-02-11 10:42:21 +0200

    feat: add challenge for February 2, 2026 - Groundhog Day

---
+++ 2026-02/2026-02-02/groundhogDay.js
@@ -0,0 +1,73 @@
+function groundhogDayPrediction(appearance) {
+
+  return appearance === true ? "Looks like we'll have six more weeks of winter."
+    : appearance === false ? "It's going to be an early spring."
+    : "No prediction this year.";
+}
+
+// --- TEST SUITE ---
+
+const testsText = String.raw`
+1. groundhogDayPrediction(true) should return "Looks like we'll have six more weeks of winter.".
+2. groundhogDayPrediction(false) should return "It's going to be an early spring.".
+3. groundhogDayPrediction(null) should return "No prediction this year.".
+4. groundhogDayPrediction(" ") should return "No prediction this year.".
+5. groundhogDayPrediction("true") should return "No prediction this year.".
+`;
+
+const testsRegex = /(?<number>\d+)\.\s(?<functionCall>.+) should return (?<output>.+?)\.?$/gm;
+const testData = [...testsText.matchAll(testsRegex).map(match => match.groups)];
+
+function runTests(testData) {
+  console.log("--------------------------");
+  console.log("🧪Starting Verification...");
+  console.log("--------------------------");
+
+  let failCount = 0;
+  
+  testData.forEach(test => {
+    const functionCallOutput = eval(test.functionCall);
+    const testOutput = eval(test.output);
+
+    const comparison = Array.isArray(testOutput)
+      ? arraysEqual(functionCallOutput, testOutput)
+      : functionCallOutput === testOutput;
+
+    if (comparison) {
+      console.log(`${test.number}.✅PASS - Function Call: ${test.functionCall}`);
+    }
+    else {
+      console.log(`${test.number}.❌FAIL - Function Call: ${test.functionCall}\nExpected: ${testOutput}\nGot: ${functionCallOutput}`);
+      ++failCount;
+    }
+  })
+
+  console.log("----------------------------",
+    failCount
+    ? `\n⚠️WARNING: ${failCount}/${testData.length} tests FAILED.`
+    : "\n🎉SUCCESS: All tests PASSED."
+  );
+}
+
+function arraysEqual(a, b) {
+  if (a.length !== b.length)
+    return false;
+
+  for (let i = 0; i < a.length; ++i) {
+    if (Array.isArray(a[i])) {
+      if (Array.isArray(b[i])) {
+        if (!arraysEqual(a[i], b[i]))
+          return false;
+      }
+      else 
+        return false;
+    }
+
+    else if (a[i] !== b[i])
+      return false;
+  }
+
+  return true;
+}
+
+runTests(testData);
