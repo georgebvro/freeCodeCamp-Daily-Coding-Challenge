20a44c7901ecddf3f385eaaf5956633a4b5c7454
GxG <georgemailro@yahoo.com>
2026-02-11 10:58:11 +0200

    feat: add challenge for February 3, 2026 - String Mirror

---
+++ 2026-02/2026-02-03/README.md
@@ -0,0 +1,12 @@
+# freeCodeCamp Daily Coding Challenge - February 3, 2026
+
+## String Mirror
+
+Given a string, return a new string that consists of the given string with a reversed copy of itself appended to the end of it.
+
+### Tests
+
+1. `mirror("freeCodeCamp")` should return `"freeCodeCamppmaCedoCeerf"`.
+2. `mirror("RaceCar")` should return `"RaceCarraCecaR"`.
+3. `mirror("helloworld")` should return `"helloworlddlrowolleh"`.
+4. `mirror("The quick brown fox...")` should return `"The quick brown fox......xof nworb kciuq ehT"`.
---
+++ 2026-02/2026-02-03/string_mirror.py
@@ -0,0 +1,41 @@
+def mirror(s):
+
+    return s + s[::-1]
+
+# --- TEST SUITE ---
+
+tests_text = r'''
+1. mirror("freeCodeCamp") should return "freeCodeCamppmaCedoCeerf".
+2. mirror("RaceCar") should return "RaceCarraCecaR".
+3. mirror("helloworld") should return "helloworlddlrowolleh".
+4. mirror("The quick brown fox...") should return "The quick brown fox......xof nworb kciuq ehT".
+'''
+
+import re
+
+tests_regex = re.compile(r"(?P<number>\d+)\.\s(?P<function_call>.+) should return (?P<output>.+?)\.?$", re.MULTILINE)
+test_data = [item.groupdict() for item in re.finditer(tests_regex, tests_text)]
+
+def run_tests(test_data):
+    print("--------------------------")
+    print("🧪 Starting Verification...")
+    print("--------------------------")
+
+    fail_count = 0
+
+    for test in test_data:
+        function_call_output = eval(test['function_call'])
+        test_output = eval(test['output'])
+        
+        if function_call_output == test_output:
+            print(f"{test['number']}.✅ PASS - Function Call:\n{test['function_call']}")
+        else:
+            print(f"{test['number']}.❌ FAIL - Function Call:\n{test['function_call']}\nExpected:\n{test_output}\nGot:\n{function_call_output}")
+            fail_count += 1
+
+    print("----------------------------",
+        f"\n⚠️ WARNING: {fail_count}/{len(test_data)} tests FAILED." if fail_count
+        else "\n🎉 SUCCESS: All tests PASSED."
+    )
+
+run_tests(test_data)
