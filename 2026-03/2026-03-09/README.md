153cfb1e684b110f9d46ffffe61cbd37c6a76dc3
GxG <georgemailro@yahoo.com>
2026-04-01 11:52:17 +0300

    feat: add challenge for March 9, 2026 - Array Sum

---
+++ 2026-03/2026-03-09/README.md
@@ -0,0 +1,13 @@
+# freeCodeCamp Daily Coding Challenge - March 9, 2026
+
+## Array Sum
+
+Given an array of numbers, return the sum of all the numbers.
+
+### Tests:
+
+1. `sumArray([1, 2, 3, 4, 5])` should return `15`.
+2. `sumArray([42])` should return `42`.
+3. `sumArray([5, -2, 7, -3])` should return `7`.
+4. `sumArray([203, 145, -129, 6293, 523, -919, 845, 2434])` should return `9395`.
+5. `sumArray([0, 0])` should return `0`.
---
+++ 2026-03/2026-03-09/arraySum.js
@@ -0,0 +1,71 @@
+function sumArray(numbers) {
+
+  return numbers.reduce((sum, number) => sum + number);
+}
+
+// --- TEST SUITE ---
+
+const testsText = String.raw`
+1. sumArray([1, 2, 3, 4, 5]) should return 15.
+2. sumArray([42]) should return 42.
+3. sumArray([5, -2, 7, -3]) should return 7.
+4. sumArray([203, 145, -129, 6293, 523, -919, 845, 2434]) should return 9395.
+5. sumArray([0, 0]) should return 0.
+`;
+
+const testsRegex = /(?<number>\d+)\.\s(?<functionCall>.+) should return (?<output>.+?)\.?$/gm;
+const testData = [...testsText.matchAll(testsRegex).map(match => match.groups)];
+
+function runTests(testData) {
+  console.log("--------------------------");
+  console.log("🧪Starting Verification...");
+  console.log("--------------------------");
+
+  let failCount = 0;
+  
+  testData.forEach(test => {
+    const functionCallOutput = eval(test.functionCall);
+    const testOutput = eval(test.output);
+
+    const comparison = Array.isArray(testOutput)
+      ? arraysEqual(functionCallOutput, testOutput)
+      : functionCallOutput === testOutput;
+
+    if (comparison) {
+      console.log(`${test.number}.✅PASS - Function Call: ${test.functionCall}`);
+    }
+    else {
+      console.log(`${test.number}.❌FAIL - Function Call: ${test.functionCall}\nExpected: ${testOutput}\nGot: ${functionCallOutput}`);
+      ++failCount;
+    }
+  })
+
+  console.log("----------------------------",
+    failCount
+    ? `\n⚠️WARNING: ${failCount}/${testData.length} tests FAILED.`
+    : "\n🎉SUCCESS: All tests PASSED."
+  );
+}
+
+function arraysEqual(a, b) {
+  if (a.length !== b.length)
+    return false;
+
+  for (let i = 0; i < a.length; ++i) {
+    if (Array.isArray(a[i])) {
+      if (Array.isArray(b[i])) {
+        if (!arraysEqual(a[i], b[i]))
+          return false;
+      }
+      else 
+        return false;
+    }
+
+    else if (a[i] !== b[i])
+      return false;
+  }
+
+  return true;
+}
+
+runTests(testData);
